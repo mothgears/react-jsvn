f13a1ef190e729c62735a6ae0ccf7a71259c142a
Inspiriton <mailbox@mothgears.com>
2020-12-04 15:53:27 +0300

    pureHTML

--- src/createReactRender.js
+++ src/createReactRender.js
@@ -1,5 +1,7 @@
-export default react => (tag, classes, params, style, events, children) => {
-	if (!children) children = [];
-	events = Object.entries(events).reduce((arr, [k, v])=>{ arr['on' + k[0].toUpperCase() + k.slice(1)] = v; return arr;}, {});
-	return react.createElement(tag, { className: classes.join(' '), style, ...events, ...params }, ...children);
-};
+export default react =>
+	(tag, classes, params, style, events, children, pureHTML = null) => {
+		if (!children) children = [];
+		if (pureHTML) params.dangerouslySetInnerHTML = pureHTML;
+		events = Object.entries(events).reduce((arr, [k, v])=>{ arr['on' + k[0].toUpperCase() + k.slice(1)] = v; return arr;}, {});
+		return react.createElement(tag, { className: classes.join(' '), style, ...events, ...params }, ...children);
+	};

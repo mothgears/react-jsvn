7027979f392cb05c7e2a3fdd6960940f3b0b7fe6
Inspiriton <mailbox@mothgears.com>
2021-01-27 17:30:12 +0300

    null children fix

--- src/createReactRender.js
+++ src/createReactRender.js
@@ -1,7 +1,7 @@
 export default react =>
 	(tag, classes, params, style, events, children, pureHTML = null) => {
 		if (!children) children = [];
-		else children = children.map(child=>child.JSVNContainer ? react.createElement(child.component, child.props) : child);
+		else children = children.map(child=>child && child.JSVNContainer ? react.createElement(child.component, child.props) : child);
 		if (pureHTML) params.dangerouslySetInnerHTML = {__html: pureHTML};
 		events = Object.entries(events).reduce((arr, [k, v])=>{ arr['on' + k[0].toUpperCase() + k.slice(1)] = v; return arr;}, {});
 		return react.createElement(tag, { className: classes.join(' '), style, ...events, ...params }, ...children);
